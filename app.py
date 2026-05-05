7ed6328840055bd2f392ca2d1d8c7575722b36e3
Gabriel-Apolinario01 <107645458+Gabriel-Apolinario01@users.noreply.github.com>
2026-05-05 18:43:43 -0300

    Add routes for compra and compras HTML pages
    Added routes for 'compra' and 'compras' pages.

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask, send_from_directory
-import os
 
 app = Flask(__name__, static_folder='.')
 
 @app.route('/')
 def index():
     return send_from_directory('.', 'index.html')
+
+@app.route('/compra')
+def compra():
+    return send_from_directory('.', 'compra.html')
+
+@app.route('/compras')
+def compras():
+    return send_from_directory('.', 'compras.html')
 
 if __name__ == '__main__':
     app.run()
